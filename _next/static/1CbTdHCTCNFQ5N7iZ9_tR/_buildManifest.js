79bd2dff2480f4041a19998b71de0331df7b99fc
FawazMulla <FawazMulla@users.noreply.github.com>
2025-12-29 17:30:16 +0000

    deploy: b455c5f480460a620c64db1c54f5ed54edc07ba1

--- _next/static/1CbTdHCTCNFQ5N7iZ9_tR/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/738-f18fff9d97361c47.js","static/chunks/pages/index-d7a05b64f62899ce.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
