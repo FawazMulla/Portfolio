920f92454b0c90e12efa19dd07655d6050dfd2b8
FawazMulla <FawazMulla@users.noreply.github.com>
2025-12-29 18:55:09 +0000

    deploy: 3371708e89e517c417f0a03cd8b9ca05534a2ded

---
+++ _next/static/4QwZ_MFKB3yg2qfCQmzym/_buildManifest.js
@@ -0,0 +1,23 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/6c36b3f42c4a0005.js"
+  ],
+  "/_error": [
+    "static/chunks/2d8684b6f4b528ac.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/Portfolio//_next/:path+",
+        "destination": "/Portfolio/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ _next/static/chunks/449927f8bbccf22b.js
@@ -0,0 +1,7 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/1596ceeeae14a82f.js",
+  "static/chunks/26986f501b8f750a.js",
+  "static/chunks/ad14c527e100952f.js",
+  "static/chunks/0bad3914ee1e0ce2.css",
+  "static/chunks/turbopack-de13ef5614d5f42c.js"
+])
