8a1295da01e132aee0c315a41ee7d70a01136986
FawazMulla <139045793+FawazMulla@users.noreply.github.com>
2025-12-29 21:12:59 +0530

    Updates

---
+++ _next/static/YHwNLvYSDt8foBz8IZqTg/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/292-acffe77e6a76aa6c.js","static/chunks/pages/index-310884de01166d93.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
